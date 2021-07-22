f2d2a638ce79951a30350b8ba7646d43ec606806
Ciaran O'Reilly <ciaran@oreilly.cat>
2021-07-22 23:01:34 +0200

    Update stt vosk plugin to latest version

--- buildroot-external/package/python-jarbas-stt-plugin-vosk/python-jarbas-stt-plugin-vosk.mk
+++ buildroot-external/package/python-jarbas-stt-plugin-vosk/python-jarbas-stt-plugin-vosk.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JARBAS_STT_PLUGIN_VOSK_VERSION = 0.1.3
+PYTHON_JARBAS_STT_PLUGIN_VOSK_VERSION = 0.2.1
 PYTHON_JARBAS_STT_PLUGIN_VOSK_SOURCE = jarbas-stt-plugin-vosk-$(PYTHON_JARBAS_STT_PLUGIN_VOSK_VERSION).tar.gz
-PYTHON_JARBAS_STT_PLUGIN_VOSK_SITE = https://files.pythonhosted.org/packages/c1/82/5a3a041d602c511cd9a611d274b9e8ce38272bb2e03a67c0aff299d7a689
+PYTHON_JARBAS_STT_PLUGIN_VOSK_SITE = https://files.pythonhosted.org/packages/0d/7f/4dd4ef8b8e5ba4389d26fbb66049b776d1c2699fd7b421ea47d4598c84c0
 PYTHON_JARBAS_STT_PLUGIN_VOSK_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
